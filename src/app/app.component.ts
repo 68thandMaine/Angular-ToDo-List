33c5a1045d8549d1f5fe0953be4161be62c30e0b
Student <example@example.com>
2019-01-24 09:45:09 -0800

    To Do List now has repeater directive and hard coded tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'The Angular To Do List';
+  currentFocus: string ="Angular Homework";
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() +1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task[] = [
+    new Task('Finish To Do List and notes'),
+    new Task('Redo Recipe box project'),
+    new Task('Complete Angular Tour of Heros Tutorial'),
+    new Task('Take notes on Thursday homework')
+  ];
 }
