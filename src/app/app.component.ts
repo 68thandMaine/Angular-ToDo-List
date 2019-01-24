3954069a770606928a37b7a0c54e4d23b4e24294
Student <example@example.com>
2019-01-24 13:07:51 -0800

    Add ngIf functionality to hide divs when finishedEditing() is activated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,36 +10,38 @@ import { Task } from './models/task.model';
 // Code below this comment is reffered to as a class declaration
 export class AppComponent {
 
-  title = 'The Angular To Do List';
-  currentFocus: string ="Angular Homework";
-  currentTime = new Date();
-  month: number = this.currentTime.getMonth() +1;
-  day: number = this.currentTime.getDate();
-  year: number = this.currentTime.getFullYear();
+title = 'The Angular To Do List';
+currentFocus: string ="Angular Homework";
+currentTime = new Date();
+month: number = this.currentTime.getMonth() +1;
+day: number = this.currentTime.getDate();
+year: number = this.currentTime.getFullYear();
 
-  tasks: Task[] = [
-    new Task('Finish To Do List and notes', 3),
-    new Task('Redo Recipe box project',2),
-    new Task('Complete Angular Tour of Heros Tutorial',2),
-    new Task('Take notes on Thursday homework',1)
-  ];
-
-
-selectedTask: Task = null;
+tasks: Task[] = [
+  new Task('Finish To Do List and notes', 3),
+  new Task('Redo Recipe box project',2),
+  new Task('Complete Angular Tour of Heros Tutorial',2),
+  new Task('Take notes on Thursday homework',1)
+];
 
 // Whenever a suser clicks on the <li> element that has the click event binding on it, the selectedTask property changes to whcihever task was clicked on. It's sorta like a set method.
-  editTask(clickedTask) {
-    this.selectedTask = clickedTask;
-  }
+selectedTask: Task = null;
+editTask(clickedTask) {
+  this.selectedTask = clickedTask;
+}
 
-  priorityColor(currentTask) {
-    if(currentTask.priority == 3) {
-      return 'bg-danger';
-    } else if (currentTask.priority == 2) {
-      return 'bg-warning';
-    } else {
-      return 'bg-info';
-    }
+priorityColor(currentTask) {
+  if(currentTask.priority == 3) {
+    return 'bg-danger';
+  } else if (currentTask.priority == 2) {
+    return 'bg-warning';
+  } else {
+    return 'bg-info';
   }
+}
+
+finishedEditing() {
+  this.selectedTask =null;
+}
 
 }
