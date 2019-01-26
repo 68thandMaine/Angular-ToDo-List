f891edf3ea741526a95a68432817f35a9032124d
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-26 14:42:04 -0800

    Add edit-task component and finish writing functionality

--- src/app/edit-task/edit-task.component.ts
+++ src/app/edit-task/edit-task.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.component.html',
   styleUrls: ['./edit-task.component.css']
 })
-export class EditTaskComponent implements OnInit {
+export class EditTaskComponent {
+  @Input() childSelectedTask: Task;
+  @Output() clickedDone = new EventEmitter;
 
-  constructor() { }
-
-  ngOnInit() {
+  finishedEditing() {
+    this.clickedDone.emit();
   }
-
 }
