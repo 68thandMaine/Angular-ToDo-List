4e7abbc67d2d84a3a17004b097dc71028e53d76d
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-26 14:48:19 -0800

    Add a new-task component

---
+++ src/app/new-task/new-task.component.ts
@@ -0,0 +1,12 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrls: ['./new-task.component.css']
+})
+export class NewTaskComponent {
+
+
+}
